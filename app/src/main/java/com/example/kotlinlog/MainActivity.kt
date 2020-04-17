9400df35bbcf418ac3a7efd5aa8a0543ce384207
mayumuiueta <mueta@koto.kpu-m.ac.jp>
2020-04-17 19:48:26 +0900

    "kotlintest", "ログへの出力テスト”を追加

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest", "ログへの出力テスト")   //ここを追加
     }
 }
