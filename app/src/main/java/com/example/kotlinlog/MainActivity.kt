de269722ce5f2991c75143b7a00d60ec358551cc
mayumuiueta <mueta@koto.kpu-m.ac.jp>
2020-04-18 20:42:28 +0900

    //レイアウトファイルを設定する

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -6,9 +6,19 @@ import android.util.Log //ここを追加
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+       super.onCreate(savedInstanceState)
+
+        //レイアウトファイルを設定する
         setContentView(R.layout.activity_main)
+
+        /*
+        複数行に
+        渡る
+        コメントを
+        記述できる
+         */
 
         Log.d("kotlintest", "ログへの出力テスト")   //ここを追加
     }
