40580924327b69b284efe3b40ec5e04fa3f021b2
mayumuiueta <mueta@koto.kpu-m.ac.jp>
2020-04-19 18:20:11 +0900

    val num1 = 10 + 5 - 2 * 4 / 2

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.kotlinlog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log //ここを追加
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,14 +11,17 @@ class MainActivity : AppCompatActivity() {
        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //整数型の変数をnumという名前で作成して、10を代入する
-        var num = 10
-        Log.d("kotlintest", Integer.toString(num))
+        val num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10 + 5 - 2 * 4 / 2 =" + num1 )
 
-        //numに50を代入する
-        //変数や定数に　.toString()を記述するほうほうもある
-        num = 50
-        Log.d("kotlintest", num. toString())
+      val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2=" + (flag1 && flag2))
+        Log.d("kotlintest", "flag1 || flag2=" + (flag1 || flag2))
+
+       val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest", "num2 < num3 =" + (num2 < num3))
 
     }
 }
