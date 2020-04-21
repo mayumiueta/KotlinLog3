8cfbd727e592a8fede68606d1fa96269a7a5d1c0
mayumuiueta <mueta@koto.kpu-m.ac.jp>
2020-04-21 16:21:04 +0900

    //引数に50と1000を指定してtotal関数を呼び出す

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -10,11 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //引数に100を指定してtotal関数を呼び出す
-        total(100)
+        //引数に50と1000を指定してtotal関数を呼び出す
+        total(50, 1000)
     }
-    //lastという名前のInt型の引数を指定する
-        private fun total(last: Int) {
+    //firstとlast、２つの引数を指定する
+        private fun total(first: Int, last: Int) {
         var sum = 0
         for ( i in 1..last)  {
             sum += i
