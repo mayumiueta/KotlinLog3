d13bb494c6f2cd0173f8db064f889de63388e151
mayumuiueta <mueta@koto.kpu-m.ac.jp>
2020-04-23 20:16:08 +0900

    val human = Human ("まゆみ", 54, "ゴルフ")

--- app/src/main/java/com/example/kotlinlog/Animal.kt
+++ app/src/main/java/com/example/kotlinlog/Animal.kt
@@ -6,7 +6,7 @@ abstract class Animal{
     var age: Int
 
     //引数付きコンストラクタ
-    constructor(name: String, age: Int, hobby: String){
+    constructor(name: String, age: Int){
     this.name = name
     this. age = age
     }
--- app/src/main/java/com/example/kotlinlog/Human.kt
+++ app/src/main/java/com/example/kotlinlog/Human.kt
@@ -6,7 +6,7 @@ open class Human: Animal, Thinkable {
     var hobby: String
 
 
-    constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
+    constructor(name: String, age: Int, hobby: String): super(name, age) {
         this. hobby = hobby
     }
 
